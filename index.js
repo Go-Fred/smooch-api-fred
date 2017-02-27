cda5fb8a7ddffd138042d4b502bb5799961bc7f2
Frederick <frederick.duberville@gmail.com>
2017-02-27 17:25:14 -0500

    Move node files to the root with env var

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 'use strict';
 
+var dotenv = require('dotenv');
+dotenv.config();
+dotenv.load();
+
 // Imports
 const express = require('express');
 const bodyParser = require('body-parser');
 const Smooch = require('smooch-core');
 
 // Config
-const PORT = 8000;
-const KEY_ID = 'your_key_id';
-const SECRET = 'your_secret_key';
+const PORT = 8001;
+const KEY_ID = process.env.KEY_ID;
+const SECRET = process.env.SECRET;
 
 const smooch = new Smooch({
     keyId: KEY_ID,
@@ -22,15 +26,18 @@ const app = express();
 app.use(bodyParser.json());
 
 // Expose /messages endpoint to capture webhooks https://docs.smooch.io/rest/#webhooks-payload
-app.post('/message', function(req, res) {
+app.post('/messages', function(req, res) {
   console.log('webhook PAYLOAD:\n', JSON.stringify(req.body, null, 4));
 
   const appUserId = req.body.appUser._id;
+  console.log(appUserId);
   // Call REST API to send message https://docs.smooch.io/rest/#post-message
   if (req.body.trigger === 'message:appUser') {
       smooch.appUsers.sendMessage(appUserId, {
-          type: 'text',
+          type: 'image',
           text: 'Live long and prosper',
+          //mediaUrl:'https://c1.staticflickr.com/6/5519/30725254545_62fc46416d_k.jpg',
+          mediaUrl:'http://data.freehdw.com/toyota-devolro-back-view.jpg',
           role: 'appMaker'
       })
           .then((response) => {
